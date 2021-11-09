902ecf6c626f732aae7e96a6776f4ec07f2b8296
Larry Garfield <larry@garfieldtech.com>
2021-11-09 10:43:59 -0600

    Disable manual creation of Feed Entries.

--- src/Controller/Admin/FeedEntryCrudController.php
+++ src/Controller/Admin/FeedEntryCrudController.php
@@ -3,6 +3,8 @@
 namespace App\Controller\Admin;
 
 use App\Entity\FeedEntry;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class FeedEntryCrudController extends AbstractCrudController
@@ -22,4 +24,12 @@ class FeedEntryCrudController extends AbstractCrudController
         ];
     }
     */
+    public function configureActions(Actions $actions): Actions
+    {
+        $actions->disable(Action::NEW);
+
+        return $actions;
+    }
+
+
 }
