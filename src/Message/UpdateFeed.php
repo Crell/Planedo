937e9fc296661e6d1addf734cca834d1d9fbf360
Larry Garfield <larry@garfieldtech.com>
2021-11-10 11:09:30 -0600

    Remove dead code.

--- src/Message/UpdateFeed.php
+++ src/Message/UpdateFeed.php
@@ -7,21 +7,4 @@ final class UpdateFeed
     public function __construct(
         public int $feedId,
     ) {}
-
-    /*
-     * Add whatever properties & methods you need to hold the
-     * data for this message class.
-     */
-
-//     private $name;
-//
-//     public function __construct(string $name)
-//     {
-//         $this->name = $name;
-//     }
-//
-//    public function getName(): string
-//    {
-//        return $this->name;
-//    }
 }
