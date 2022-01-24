f8d597d18b35f96a27b8c1f22b40e379aa35fb59
Larry Garfield <larry@garfieldtech.com>
2022-01-24 10:18:28 -0600

    Add a form for users to edit their own accounts.

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -10,10 +10,12 @@ use App\Repository\FeedRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -52,8 +54,33 @@ class DashboardController extends AbstractDashboardController
                 ->setBadge($this->entryRepo->getApprovedEntryCount()),
 
             MenuItem::section('Users'),
-//            MenuItem::linkToCrud('Comments', 'fa fa-comment', Comment::class),
             MenuItem::linkToCrud('Users', 'fa fa-user', User::class),
         ];
     }
+
+    public function configureUserMenu(UserInterface $user): UserMenu
+    {
+        // Usually it's better to call the parent method because that gives you a
+        // user menu with some menu items already created ("sign out", "exit impersonation", etc.)
+        // if you prefer to create the user menu from scratch, use: return UserMenu::new()->...
+        return parent::configureUserMenu($user)
+            // use the given $user object to get the user name
+            //->setName($user->getFullName())
+            // use this method if you don't want to display the name of the user
+            //->displayUserName(false)
+
+            // you can return an URL with the avatar image
+            //->setAvatarUrl('https://...')
+            //->setAvatarUrl($user->getProfileImageUrl())
+            // use this method if you don't want to display the user image
+            //->displayUserAvatar(false)
+            // you can also pass an email address to use gravatar's service
+            //->setGravatarEmail($user->getMainEmailAddress())
+
+            // you can use any type of menu item, except submenus
+            ->addMenuItems([
+                MenuItem::linkToRoute('Profile', 'fa fa-id-card', 'user_settings',),
+            ]);
+    }
+
 }
